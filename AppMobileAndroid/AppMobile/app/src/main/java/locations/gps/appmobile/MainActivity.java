63771b57bf083fd168a3363e944b091a400d92f2
Foloba Anca <foloba.anca@yahoo.com>
2016-11-08 01:29:22 +0200

    Sending data via gmail in android studio and setting the new project for react native.

--- AppMobileAndroid/AppMobile/app/src/main/java/locations/gps/appmobile/MainActivity.java
+++ AppMobileAndroid/AppMobile/app/src/main/java/locations/gps/appmobile/MainActivity.java
@@ -1,12 +1,15 @@
 package locations.gps.appmobile;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,25 +26,46 @@ public class MainActivity extends AppCompatActivity
         EditText l_editTextLocationName=(EditText)findViewById(R.id.editTextLocationName);
         String l_locationName=l_editTextLocationName.getText().toString();
 
-        Log.w("myTag", l_locationName);
-
-        AlertDialog l_alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-        l_alertDialog.setTitle("Submit");
-        l_alertDialog.setMessage("Request submitted!");
-        l_alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
-                new DialogInterface.OnClickListener()
-                {
-                    public void onClick(DialogInterface f_dialog, int f_which)
-                    {
-                        f_dialog.dismiss();
-
-                        finish();
-                        startActivity(getIntent());
-                    }
-                });
-        l_alertDialog.show();
+        EditText l_editTextDescription=(EditText)findViewById(R.id.editTextDescription);
+        String l_description=l_editTextDescription.getText().toString();
 
         String l_subjectEmail = "New location request";
         String l_bodyEmail = "";
+
+        l_bodyEmail += "Location name: " + l_locationName;
+        l_bodyEmail += "\n";
+        l_bodyEmail += "Description: " + l_description;
+
+        String l_toEmail = "foloba.anca@gmail.com";
+
+        Intent l_email = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", l_toEmail, null
+        ));
+        l_email.putExtra(Intent.EXTRA_SUBJECT, l_subjectEmail);
+        l_email.putExtra(Intent.EXTRA_TEXT, l_bodyEmail);
+
+        try
+        {
+            startActivity(l_email);
+            AlertDialog l_alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            l_alertDialog.setTitle("Submit");
+            l_alertDialog.setMessage("Request submitted!");
+            l_alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                    new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface f_dialog, int f_which)
+                        {
+                            f_dialog.dismiss();
+
+                            finish();
+                            startActivity(getIntent());
+                        }
+                    });
+            l_alertDialog.show();
+        }
+        catch (android.content.ActivityNotFoundException f_exception)
+        {
+            Toast.makeText(MainActivity.this, "The email is invalid!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
